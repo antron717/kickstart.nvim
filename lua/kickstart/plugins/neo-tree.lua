702c167f2ebfb65e769c96180a167fabbbdd5ffc
Anthony <67001452+antron717@users.noreply.github.com>
2024-05-24 20:13:22 +0100

    update neo-tree/gitsigns

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,6 +15,34 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = false, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = true,
+        hide_gitignored = true,
+        hide_hidden = false, -- only works on Windows for hidden files/directories
+        hide_by_name = {
+          'node_modules',
+        },
+        hide_by_pattern = { -- uses glob style patterns
+          --"*.meta",
+          --"*/src/*/tsconfig.json",
+        },
+        always_show = { -- remains visible even if other settings would normally hide it
+          --".gitignored",
+        },
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+          '.DS_Store',
+          --"thumbs.db"
+        },
+        never_show_by_pattern = { -- uses glob style patterns
+          --".null-ls_*",
+        },
+      },
+      follow_current_file = {
+        enabled = false, -- This will find and focus the file in the active buffer every time
+        --               -- the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
